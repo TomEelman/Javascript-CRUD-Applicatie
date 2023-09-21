5b5aa7bdee1e57984f2505fbe65bc27be31c61c6
TomEelmna <Tomeelman33@gmail.com>
2023-09-21 13:21:49 +0200

    Commit na feedback

--- js/indexscript.js
+++ js/indexscript.js
@@ -1,34 +1,75 @@
-if (document.URL.includes("create.html")) {
-  let userData = JSON.parse(localStorage.getItem('personnel')) || [];
+const idInput = document.getElementById('idInput');
+const firstnameInput = document.getElementById('firstname');
+const infixInput = document.getElementById('infix');
+const lastnameInput = document.getElementById('lastname');
+const cityInput = document.getElementById('city');
+const postalcodeInput = document.getElementById('postalcode');
+const streetInput = document.getElementById('street');
+const streetnumberInput = document.getElementById('streetnumber');
+const additiveInput = document.getElementById('additive');
+const userForm = document.getElementById('userForm');
+const CreatePersonModal = document.getElementById('CreatePersonModal');
+const submitButtonCreate = document.getElementById('submitButtonCreate');
+const submitButtonEdit = document.getElementById('submitButtonEdit');
 
-  const SaveData = (ev) => {
-    ev.preventDefault();
+init()
+function init() {
+  displayPersonnelData()
+  if (userForm) {
+    userForm.style.display = "block";
+    submitButtonEdit.style.display = "none";
+  }
+}
 
-    const firstnameInput = document.getElementById('firstname');
-    const infixInput = document.getElementById('infix');
-    const lastnameInput = document.getElementById('lastname');
-    const cityInput = document.getElementById('city');
-    const postalcodeInput = document.getElementById('postalcode');
-    const streetInput = document.getElementById('street');
-    const streetnumberInput = document.getElementById('streetnumber');
-    const additiveInput = document.getElementById('additive');
+function getUsersFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('personnel')) || [];
+}
 
-    // Check to see if input is empty
-    if (
-      !firstnameInput.value ||
-      !lastnameInput.value ||
-      !cityInput.value ||
-      !postalcodeInput.value ||
-      !streetInput.value ||
-      !streetnumberInput.value
-    ) {
-      // If it is empty give this error message
-      alert('Vul alle velden met een * in');
-      return;
-    }
+function showEditUserForm(ev) {
+  const userId = ev.target.attributes["data-userid"].value;
+  const users = getUsersFromLocalStorage();
+  const userIndex = users.findIndex((users) => users.Id == userId);
+  if (userForm) {
+    userForm.style.display = "block";
+    submitButtonCreate.style.display = "none";
+    fillUserForm(users[userIndex])
+  }
+  if (userId) {
+    submitButtonEdit.style.display = "block";
+  }
+}
 
-    //Fill the object if all input fields are filled
-    let userdata = {
+function fillUserForm(user) {
+  idInput.value = user.Id;
+  firstnameInput.value = user.Firstname;
+  infixInput.value = user.Infix;
+  lastnameInput.value = user.Lastname;
+  cityInput.value = user.City;
+  postalcodeInput.value = user.PostalCode;
+  streetInput.value = user.Street;
+  streetnumberInput.value = user.Streetnumber;
+  additiveInput.value = user.Additive;
+}
+
+function validateUserData() {
+  let isValid = true;
+  if (
+    !firstnameInput.value ||
+    !lastnameInput.value ||
+    !cityInput.value ||
+    !postalcodeInput.value ||
+    !streetInput.value ||
+    !streetnumberInput.value
+  ) {
+    isValid = false
+    alert("Vul alle velden met een * in")
+  }
+  return isValid;
+}
+
+function addUser() {
+  if (validateUserData(true)) {
+    let user = {
       Id: Date.now(),
       Firstname: firstnameInput.value,
       Infix: infixInput.value,
@@ -40,90 +81,70 @@ if (document.URL.includes("create.html")) {
       Additive: additiveInput.value,
     }
 
-    // Push to local storage and redirect
-    userData.push(userdata);
+    const userData = getUsersFromLocalStorage()
+
+    userData.push(user);
+
     localStorage.setItem('personnel', JSON.stringify(userData));
-    location.replace('index.html');
+
   }
+}
 
-  //Edit User
-  //Look in the Local storage key "editPerson"
-  const editPersonData = localStorage.getItem("editPerson");
+function updateUser() {
+  closeModal();
+  if (validateUserData(true)) {
+    const editUserId = idInput.value;
+    const userData = getUsersFromLocalStorage();
+    const editUserIndex = userData.findIndex((user) => user.Id == editUserId);
 
-  if (editPersonData) {
-    // Parse
-    const editPerson = JSON.parse(editPersonData);
-    // Fills the input fields with the existing information
-    document.getElementById("firstname").value = editPerson.Firstname;
-    document.getElementById("infix").value = editPerson.Infix;
-    document.getElementById("lastname").value = editPerson.Lastname;
-    document.getElementById("city").value = editPerson.City;
-    document.getElementById("postalcode").value = editPerson.PostalCode;
-    document.getElementById("street").value = editPerson.Street;
-    document.getElementById("streetnumber").value = editPerson.Streetnumber;
-    document.getElementById("additive").value = editPerson.Additive;
-  }
+    if (editUserIndex !== -1) {
+      userData[editUserIndex] = {
+        Id: editUserId,
+        Firstname: firstnameInput.value,
+        Infix: infixInput.value,
+        Lastname: lastnameInput.value,
+        City: cityInput.value,
+        PostalCode: postalcodeInput.value,
+        Street: streetInput.value,
+        Streetnumber: streetnumberInput.value,
+        Additive: additiveInput.value,
+      };
 
-  // Set the checklist image based on edit or create mode
-  if (editPersonData) {
-    document.getElementById('form-img').src = 'img/checklistedit.png';
-  } else {
-    document.getElementById('form-img').src = 'img/checklistcreate.png';
-  }
-
-  const submitButton = document.getElementById('btn');
-
-  // If the edit person object is filled the button will change to "Aapassen" 
-  //and when you click it it will edit the user info then delte the editPerson key out of localstorage so you can still create users
-  if (editPersonData) {
-    submitButton.textContent = "Aanpassen";
-    localStorage.removeItem("editPerson");
-  }
-
-  // If editPersonData is active it will edit the user
-  submitButton.addEventListener('click', function (ev) {
-    ev.preventDefault();
-    if (editPersonData) {
-      Edit(JSON.parse(editPersonData));
-    } else {
-      SaveData(ev);
+      localStorage.setItem('personnel', JSON.stringify(userData));
     }
-  });
-}
-
-function Edit(person) {
-  let personnelData = JSON.parse(localStorage.getItem("personnel")) || [];
-
-  const editIndex = personnelData.findIndex((element) => element.Id === person.Id);
-
-  if (editIndex !== -1) {
-    // Edits Information
-    personnelData[editIndex].Firstname = document.getElementById('firstname').value;
-    personnelData[editIndex].Infix = document.getElementById('infix').value;
-    personnelData[editIndex].Lastname = document.getElementById('lastname').value;
-    personnelData[editIndex].City = document.getElementById('city').value;
-    personnelData[editIndex].PostalCode = document.getElementById('postalcode').value;
-    personnelData[editIndex].Street = document.getElementById('street').value;
-    personnelData[editIndex].Streetnumber = document.getElementById('streetnumber').value;
-    personnelData[editIndex].Additive = document.getElementById('additive').value;
-    // Local storage update
-    localStorage.setItem("personnel", JSON.stringify(personnelData));
-
-    // Redirect
-    location.replace("index.html");
   }
 }
 
-if (document.URL.includes("index.html")) {
-  function showModal(person) {
+function deleteUser(userId) {
+  const userData = getUsersFromLocalStorage();
+  const deleteUserIndex = userData.findIndex((user) => user.Id == userId);
 
-    //More information function 
-    const modal = document.createElement("div");
-    modal.className = "person-modal";
+  alert('WAARSCHUWING Weet je het zeker?')
+  userData.splice(deleteUserIndex, 1);
+  localStorage.setItem('personnel', JSON.stringify(userData));
+  location.reload()
+}
 
-    //Modal structure
-    modal.innerHTML = `
-      <div class="modal-background modal-visible">
+submitButtonCreate.addEventListener('click', function () {
+  addUser();
+});
+
+submitButtonEdit.addEventListener('click', function () {
+  updateUser()
+});
+
+function showModal(person) {
+
+  const modal = document.createElement("div");
+  modal.className = "person-modal";
+
+  const personModal = document.createElement("div");
+  personModal.className = "person-modal";
+  document.body.appendChild(personModal);
+
+
+  modal.innerHTML =
+    `<div class="modal-background modal-visible">
         <div class="modal">
           <span class="close" onclick="closeModal()">&times;</span>
           <h2>Details van ${person.Firstname} ${person.Lastname}</h2>
@@ -131,161 +152,81 @@ if (document.URL.includes("index.html")) {
           <p>Volledige naam: ${person.Firstname} ${person.Infix} ${person.Lastname}</p>
           <p>Woonplaats & Postcode: ${person.City}, ${person.PostalCode}</p>
           <p>Straat en Straatnummer: ${person.Street}, ${person.Streetnumber}${person.Additive}</p>
-          <button id="EditButtonInModal" class="edit-button-modal">Personeels gegevens aanpassen<i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
           <button id="DeleteButtonInModal" class="delete-button-modal">Persoon verwijderen<i class="fa fa-trash-o" aria-hidden="true"></i></button>
         </div>
-      </div>
-    `;
-    document.body.appendChild(modal);
+      </div>`;
+  document.body.appendChild(modal);
 
-    // Get info out of local storage
-    var personnelData = localStorage.getItem("personnel");
+  var deleteButtonInModal = document.getElementById("DeleteButtonInModal");
+  deleteButtonInModal.onclick = () => deleteUser(person.Id);
+}
 
-    // parse
-    var personnelObject = JSON.parse(personnelData);
-
-    var list = personnelObject.findIndex(function (element) {
-      return element.Id === person.Id;
-    });
-
-    //Loop 
-    for (var i = 0; i < personnelObject.length; i++) {
-
-      // Edit Function For button in modal 
-      var EditButtonInModal = document.getElementById("EditButtonInModal");
-      EditButtonInModal.onclick = () => Edit(person);
-
-      // Delete Function For button in modal 
-      var deleteButtonInModal = document.getElementById("DeleteButtonInModal");
-      deleteButtonInModal.onclick = () => Delete(person);
-      
-    }
+function closeModal() {
+  const modals = document.querySelectorAll(".person-modal");
+  for (const modal of modals) {
+    modal.innerHTML = "";
   }
+}
 
-  // Function to close modal with the x
-  function closeModal() {
-    const modals = document.querySelectorAll(".person-modal");
-    for (const modal of modals) {
-      modal.innerHTML = "";
-    }
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal-background")) {
+    closeModal();
   }
-  // Function to close modal with clicking on the background
-  window.onclick = function (event) {
-    if (event.target.classList.contains("modal-background")) {
-      closeModal();
-    }
-  }
+}
 
-  //Delete function 
-  function Delete(person) {
-    var personnelData = localStorage.getItem("personnel");
-    var personnelObject = JSON.parse(personnelData);
-    var list = personnelObject.findIndex(function (element) {
-      return element.Id === person.Id;
-    });
+function displayPersonnelData() {
 
-    if (list !== -1) {
-      // Deletes user from the Object
-      personnelObject.splice(list, 1);
-      // Update the local storage
-      localStorage.setItem("personnel", JSON.stringify(personnelObject));
+  userList = getUsersFromLocalStorage()
 
-      // Delete's user from the list
-      var personDiv = document.getElementById("person_" + person.Id);
-      if (personDiv) {
-        personDiv.remove();
-        location.reload();
-      }
-    }
-  }
+  var personnelList = document.getElementById("first-lastnameList");
 
-  function Edit(person) {
+  if (userList.length === 0) {
 
-    localStorage.setItem("editPerson", JSON.stringify(person));
+    document.getElementById("no-users-message").innerHTML = ("Geen gebruikers gevonden");
 
-    location.replace("create.html");
+  } else {
 
-  }
+    for (var i = 0; i < userList.length; i++) {
+      const Id = userList[i].Id;
+      const firstname = userList[i].Firstname;
+      const lastname = userList[i].Lastname;
+      const infix = userList[i].Infix;
 
-  // Make a modal for every added person
-  const personModal = document.createElement("div");
-  personModal.className = "person-modal";
-  document.body.appendChild(personModal);
+      var personDiv = document.createElement("ul");
+      personDiv.id = "person_" + Id;
+      personDiv.className = "person_block";
 
-  function displayPersonnelData() {
-    // Get info out of local storage
-    var personnelData = localStorage.getItem("personnel");
+      var firstnameElement = document.createElement("p");
+      firstnameElement.textContent = "Naam: " + firstname + " " + infix + " " + lastname;
 
-    // parse
-    var personnelObject = JSON.parse(personnelData);
+      var idElement = document.createElement("p");
+      idElement.textContent = "ID:" + Id;
 
-    // Where all the information is being displayed
-    var personnelList = document.getElementById("first-lastnameList");
+      var infoIcon = document.createElement("i");
+      infoIcon.className = "fa fa-info";
+      infoIcon.addEventListener("click", (function (person) {
+        return function () {
+          showModal(person);
+        };
+      })
+        (userList[i]));
 
-    if (personnelObject.length === 0) {
-      document.getElementById("no-users-message").innerHTML = ("Geen gebruikers gevonden");
-    } else {
+      var potloodIcon = document.createElement("i");
+      potloodIcon.className = "fa fa-pencil";
+      potloodIcon.setAttribute("data-userId", `${Id}`);
+      potloodIcon.addEventListener("click", showEditUserForm);
 
-      for (var i = 0; i < personnelObject.length; i++) {
-        var Id = personnelObject[i].Id;
-        var firstname = personnelObject[i].Firstname;
-        var lastname = personnelObject[i].Lastname;
-        var infix = personnelObject[i].Infix;
+      var trashIcon = document.createElement("i");
+      trashIcon.className = "fa fa-trash";
+      trashIcon.addEventListener("click", deleteUser);
 
-        // Make a block for every added person
-        var personDiv = document.createElement("ul");
-        personDiv.id = "person_" + Id;
-        personDiv.className = "person_block";
 
-        //Trash icon / Delete Function
-        var trashIcon = document.createElement("i");
-        trashIcon.className = "fa fa-trash";
-        trashIcon.addEventListener("click", (function (deleteperson) {
-          return function () {
-            Delete(deleteperson);
-          };
-        })
-          (personnelObject[i])
-        );
-
-        //Pencil icon / Edit Function
-        var potloodIcon = document.createElement("i");
-        potloodIcon.className = "fa fa-pencil";
-        potloodIcon.addEventListener("click", (function (editperson) {
-          return function () {
-            Edit(editperson);
-          };
-        })
-          (personnelObject[i])
-        );
-
-        //More information icon / More information Function
-        var infoIcon = document.createElement("i");
-        infoIcon.className = "fa fa-info";
-        infoIcon.addEventListener("click", (function (person) {
-          return function () {
-            showModal(person);
-          };
-        })
-          (personnelObject[i])
-        );
-
-        var firstnameElement = document.createElement("p");
-        firstnameElement.textContent = "Naam: " + firstname + " " + infix + " " + lastname;
-
-        var idElement = document.createElement("p");
-        idElement.textContent = "ID:" + Id;
-
-        // icons
-        personDiv.appendChild(trashIcon);
-        personDiv.appendChild(potloodIcon);
-        personDiv.appendChild(infoIcon);
-        personDiv.appendChild(firstnameElement);
-        personDiv.appendChild(idElement);
-        personnelList.appendChild(personDiv);
-      }
+      personDiv.appendChild(trashIcon);
+      personDiv.appendChild(potloodIcon);
+      personDiv.appendChild(infoIcon);
+      personDiv.appendChild(firstnameElement);
+      personDiv.appendChild(idElement);
+      personnelList.appendChild(personDiv);
     }
   }
 }
-// If the page loads displayPersonnelData
-window.addEventListener("load", displayPersonnelData);
